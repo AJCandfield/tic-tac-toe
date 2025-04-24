0cb5814849f3e5f72bad42fb190452776dad2fce
Alexander James Candfield <alex.candfield@gmail.com>
2025-04-24 15:03:11 +0100

    minor

--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"container/ring"
 	"fmt"
+	"log/slog"
+	"os"
 )
 
 func main() {
-
-	// Create 3x3 grid
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("start of execution")
+	// Create 3x3 grid.
+	// Horizontal lines are contained in the vertical lines.
+	//
+	// Therefore, grid[y][x]
+	//
+	// Example: grid[0]    == the first row of squares
+	// Example: grid[1][2] == the second row of squares, the third square
 	grid := [][]string{
-		{" ", " ", " "},
-		{" ", " ", " "},
-		{" ", " ", " "},
+		// <--x-axis -->
+		{" ", " ", " "}, // }
+		{" ", " ", " "}, // } -> y-axis
+		{" ", " ", " "}, // }
 	}
 
 	// Create a ring buffer to host the player's symbols
@@ -22,29 +32,12 @@ func main() {
 	r.Value = "o"
 
 	for {
-		var player string
-
 		// Get the current player's symbol
-		player = r.Value.(string)
+		player := r.Value.(string)
+		log_turn := logger.With("player", player)
+		log_turn.Debug("new turn started")
 
-		var xCord int
-		var yCord int
-
-		println("Current playa:", player)
-
-		fmt.Print("Enter position (x): ")
-		fmt.Scan(&xCord)
-
-		fmt.Print("Enter position (y): ")
-		fmt.Scan(&yCord)
-
-		if grid[yCord-1][xCord-1] != " " {
-			println("Illegal move!")
-			continue
-		}
-
-		grid[yCord-1][xCord-1] = player
-
+		// Draw grid with updated symbols
 		// For each row
 		for y := 0; y < len(grid); y++ {
 			// For each column
@@ -53,6 +46,83 @@ func main() {
 			}
 			println()
 		}
+		log_turn.Debug("printed grid")
+
+		var xCord int
+		var yCord int
+
+		println("Current player:", player)
+
+		fmt.Print("Enter position (column): ")
+		fmt.Scan(&xCord)
+
+		fmt.Print("Enter position (row): ")
+		fmt.Scan(&yCord)
+
+		// Check if selected square is free
+		if grid[yCord-1][xCord-1] != " " {
+			println("Illegal move!")
+			continue
+		}
+
+		// Assign player symbol to selected square
+		grid[yCord-1][xCord-1] = player
+
+		// === DETECT WIN ===
+		//
+		// there are 3 win conditions
+		// - Any horizontal line (3)
+		// - Any vertical line (3)
+		// - Any diagonal line (2)
+		//
+		// We check the current symbol/player in each square
+		// We break the check when the symbol/player doesn't match
+
+		// Check horizontal win
+		for y := 0; y < len(grid); y++ {
+			for x := 0; x < len(grid[y]); x++ {
+				if grid[y][x] != player {
+					break
+				}
+				if x == len(grid[y])-1 {
+					fmt.Println("Player", player, "won!")
+				}
+			}
+		}
+
+		// Check vertical win
+		for x := 0; x < len(grid); x++ {
+			for y := 0; y < len(grid[x]); y++ {
+				if grid[x][y] != player {
+					break
+				}
+				if x == len(grid[x])-1 {
+					fmt.Println("Player", player, "won!")
+				}
+			}
+		}
+
+		// Check first diagonal win
+		for z := 0; z < len(grid); z++ {
+			if grid[z][z] != player {
+				break
+			}
+			if z == len(grid)-1 {
+				fmt.Println("Player", player, "won!")
+			}
+		}
+
+		// Check second diagonal win
+		for z := 0; z < len(grid); z++ {
+			if grid[z][(len(grid)-1)-z] != player {
+				break
+			}
+			if z == len(grid)-1 {
+				fmt.Println("Player", player, "won!")
+			}
+		}
+
+		// Call next turn
 		r = r.Next()
 	}
 }
